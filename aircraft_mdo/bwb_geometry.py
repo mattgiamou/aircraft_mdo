e7550e492f2a0cc38bdca8e06d19eaf7e57fe6f8
matt.hermes <matthew.giamou@gmail.com>
2015-03-01 19:22:24 -0500

    Playing with cost function.

--- aircraft_mdo/bwb_geometry.py
+++ aircraft_mdo/bwb_geometry.py
@@ -4,3 +4,11 @@
 from openmdao.main.api import Component
 from openmdao.main.datatypes.api import Float
 
+def compute_mass():
+    """ Used to estimate the empty mass of the plane. Should scale up with 
+    surface area of body and wings in some principled manner. 
+    """
+    electronics = 0.3
+    
+    mass = electronics
+    return mass
