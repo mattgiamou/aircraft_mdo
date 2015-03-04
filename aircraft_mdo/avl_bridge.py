3beb69a4c07ad7852de45b6a6c9fc8a0bbfae8ea
Matt Giamou <matthew.giamou@gmail.com>
2015-03-04 00:32:06 -0500

    AVL bridge parameter setting working.

--- aircraft_mdo/avl_bridge.py
+++ aircraft_mdo/avl_bridge.py
@@ -1,31 +1,50 @@
 """
-AVL bridge using pexpect.
+AVL bridge using pexpect library.
 """
 import pexpect
 
+# Reg. expression for AVL console
 avl_expect = '[crs]>'
+default_file = 'avl/eppler330_config_no_tail'
 
 class AVL():
+    # Reg. expression for AVL console
     avl_expect = '[crs]>'
+    default_file = 'avl/eppler330_config_no_tail'
     
-    def __init__(self, avl_filename):
+    def __init__(self, avl_filename=default_file):
         """
         Construct a new bridge each time the geometry is modified. Use the 
         'with' statement to avoid leaving the process running.
         """
         self.p = pexpect.spawn('avl ' + avl_filename)
+        # Do I need to set oper each time?
+        self.p.sendline('oper')
+        self.p.expect(self.avl_expect)
+        
+    def expect(self, token=avl_expect):
+        self.p.expect(token)
         
     def set_parameter(self, param, value):
         """
         Sets a parameter in AVL.
         """
-        pass
+        self.p.sendline('m')
+        self.expect()
+        self.p.sendline(param)
+        self.expect()
+        self.p.sendline(str(value))
+        self.expect()
+        self.p.sendline('')
+        self.expect()
+        return self.p.before
     
     def set_constraint(self, param, constrainer, constraint):
         """
         Sets a constraint in AVL.
         E.g.: param = 'a', constrainer = 'c', constraint = 0.2 would constrain
-        alpha such that CL = 0.2
+        alpha such that CL = 0.2. May need to cache the set value because it 
+        needs to be expect()-ed (check this)
         """
         pass
     
@@ -75,5 +94,7 @@ def pexpect_attempt():
     print "\nClosed process."
     
 if __name__ == '__main__':
-    pexpect_attempt()
-    
+    #pexpect_attempt()
+    avl = AVL()
+    output = avl.set_parameter('CD', 0.02)
+    print output
