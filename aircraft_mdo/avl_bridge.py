a747ccfa311c588b3e2b3840b818e9df141c06fe
Matt Giamou <matthew.giamou@gmail.com>
2015-03-04 02:16:04 -0500

    First test of output reading is a go: extracted CL, CM and CD curves. Next are stability derivatives, then need to work on the file input side of things: .avl and .mass file generation.

--- aircraft_mdo/avl_bridge.py
+++ aircraft_mdo/avl_bridge.py
@@ -5,6 +5,8 @@ AVL must be installed and on the path.
 
 import pexpect
 import re
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Reg. expression for AVL console
 avl_expect = '[crs]>'
@@ -56,14 +58,20 @@ class AVL():
         """
         self.p.sendline('x')        
         self.expect()
-        return self.p.before
+        output = self.p.before
+        CL = self.get_output_val(output, 'CLtot')
+        CD = self.get_output_val(output, 'CDtot')
+        CM = self.get_output_val(output, 'Cmtot')
+        
+        return (CL, CD, CM)
+        
         
     def get_output_val(self, output, var):
         """
         Extract variable 'var' from string output.
         """
-        # Needs +,-,., and E for exponential notation
-        match1 = re.search(str(var)+' =[ ]+[\.\-\+0-9E]+', run_output)
+        # Needs +,-,., and E (for exponential notation)
+        match1 = re.search(str(var)+' =[ ]+[\.\-\+0-9E]+', output)
         subquery = output[match1.start():match1.end()]
         # Extract the number from the sub-query
         submatch = re.search('[\.\-\+0-9E]+', subquery)
@@ -78,7 +86,8 @@ class AVL():
         self.p.close(force=True)
         
 if __name__ == '__main__':
-    #Test basic functionality
+    # Test basic functionality (with ensures that the AVL process is closed on 
+    # exit)
     with AVL() as avl:
         param_output = avl.set_parameter('CD', 0.02)
         print param_output
@@ -88,5 +97,27 @@ if __name__ == '__main__':
         print constraint_output
         run_output = avl.run()
         print run_output
-        zref = avl.get_output_val(run_output, 'Zref')
-        print zref
+        #zref = avl.get_output_val(run_output, 'Zref')
+        #print zref
+        
+    with AVL() as avl:
+        N = 11
+        alpha = np.linspace(0, 10, N)
+        CL = np.zeros(alpha.shape)
+        CD = np.zeros(CL.shape)
+        CM = np.zeros(CL.shape)
+        
+        for idx in xrange(0, N):
+            avl.set_constraint('a','a',alpha[idx])
+            cl,cd,cm = avl.run()
+            CL[idx] = cl
+            CD[idx] = cd
+            CM[idx] = cm
+        plt.figure()
+        plt.plot(alpha, CL)
+        plt.grid()
+        plt.plot(alpha,CD)
+        plt.plot(alpha, CM)
+        plt.xlabel('alpha (deg)')
+        plt.legend(('CL','CD','CM'))
+        
